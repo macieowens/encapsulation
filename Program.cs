0e0ea70a5e3654923f0aaf489d9eb8876154b5da
macieowens <112588931+macieowens@users.noreply.github.com>
2022-11-26 16:59:36 -0600

    initial commit'

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bankaccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bankAccountMacie ba = new bankAccountMacie();
+
+            Console.WriteLine("How much $$$ do you want to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current balance is {userBalance,0:c}");
+
+        }
+    }
+}
